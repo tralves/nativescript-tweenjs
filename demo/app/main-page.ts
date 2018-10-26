92a440ba1a963337ed6f9c4218bfcf26d5d6ce70
Brad Martin <bradwaynemartin@gmail.com>
2018-10-26 10:35:53 -0500

    button width demo

--- demo/app/main-page.ts
+++ demo/app/main-page.ts
@@ -1,19 +1,20 @@
-import * as observable from 'tns-core-modules/data/observable'
-import * as pages from 'tns-core-modules/ui/page'
-import { HelloWorldModel } from './main-view-model'
+import * as observable from "tns-core-modules/data/observable";
+import * as pages from "tns-core-modules/ui/page";
+import { HelloWorldModel } from "./main-view-model";
+import { TWEEN } from "nativescript-tweenjs";
 
 // Event handler for Page 'loaded' event attached in main-page.xml
 export function pageLoaded(args: observable.EventData) {
   // Get the event sender
-  let page = <pages.Page>args.object
-  page.bindingContext = new HelloWorldModel()
+  let page = <pages.Page>args.object;
+  page.bindingContext = new HelloWorldModel();
 
   var tween = new TWEEN.Tween({ x: 50 })
     .to({ x: 400 }, 5000)
     .easing(TWEEN.Easing.Elastic.InOut)
-    .onUpdate(function(object) {
-      page.bindingContext.set('message', Math.round(object.x))
-      page.bindingContext.set('bungee', object.x)
+    .onUpdate(object => {
+      page.bindingContext.set("message", Math.round(object.x));
+      page.bindingContext.set("bungee", object.x);
     })
-    .start()
+    .start();
 }
